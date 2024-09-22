22c4963ea0e996ba5e11f7e61155493c34046f81
ThuanHM <thuanhmdev@gmail.com>
2024-09-22 18:30:18 +0700

    create project
    Signed-off-by: ThuanHM <thuanhmdev@gmail.com>

---
+++ src/main/java/com/themeshop/themeshop/ThemeshopApplication.java
@@ -0,0 +1,13 @@
+package com.themeshop.themeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/themeshop/themeshop/ThemeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themeshop.themeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
